1f3c263004f76ca4b900080a0cec9473dd9b19f2
Chau <tranhoaichau.00@gmail.com>
2021-05-17 12:01:07 +0700

    fix adroid bug

--- plant_care_fe/lib/main.dart
+++ plant_care_fe/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/home_screen.dart';
